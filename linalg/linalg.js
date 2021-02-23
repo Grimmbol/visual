93b64583a6ba7d60ce6bb36b4a97494d620195b2
Grimmbol <krogstie.oystein@gmail.com>
2021-02-23 20:36:05 +0100

    move line drawing into own primitive, add arrowhead to drawn vectors

--- linalg/linalg.js
+++ linalg/linalg.js
@@ -8,7 +8,7 @@ const STROKE_VECTOR  = 'rgb(255,  0,  0)';
 // *** Rendring ***
 function render(state) {
     draw_axis(state);
-    render_objects(state)
+    render_objects(state);
 }
 
 function render_objects(state) {
@@ -19,7 +19,6 @@ function render_objects(state) {
 	let current_type = current_object.type
 	
 	if(string_keys_match(current_type, "vector")) {
-	    console.log("render a vector")
 	    render_vector(
 		state,
 		current_object.x,
@@ -41,10 +40,6 @@ function draw_axis(state) {
     let originy = state.origin.y;
     let width   = state.size.width;
     let height  = state.size.height;
-
-    console.log(originx, originy, width, height);
-    
-    console.log(context)
     
     context.beginPath();
     context.moveTo(originx, 0);
@@ -56,20 +51,52 @@ function draw_axis(state) {
 }
 
 function render_vector(state, x, y, start) {
+
+    // The base line
+    renderLine(state, STROKE_VECTOR, x, y, start);
+    
+    // The arrowhead. This should be a right angle, with the line at 45 degrees
+    let baseAngle = 0
+    baseAngle = Math.asin (
+	y / Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+    )
+
+    console.log('baseAngle in rads:', baseAngle);
+    baseAngle = (baseAngle/(2*Math.PI)) * 360;
+    
+    console.log('baseAngle:', baseAngle);
+
+    let upperAngle = baseAngle + 180 - 45;
+    let lowerAngle = baseAngle + 180 + 45;
+    let baseLength = 10;
+
+    console.log(upperAngle, lowerAngle);
+    
+    let baseVector = {x: baseLength, y:0};
+    let upperVector = rotateVector(baseVector.x, baseVector.y, upperAngle);
+    let lowerVector = rotateVector(baseVector.x, baseVector.y, lowerAngle);
+
+    console.log(baseVector, upperVector, lowerVector);
+
+    let tipx = start.x + x;
+    let tipy = start.y + y;
+    
+    renderLine(state, STROKE_VECTOR, upperVector.x, upperVector.y, {x: tipx, y: tipy});
+    renderLine(state, STROKE_VECTOR, lowerVector.x, lowerVector.y, {x: tipx, y: tipy});
+}
+
+function renderLine(state, stroke, x, y, start) {
     let context = state.context;
     let offsetx = state.origin.x;
     let offsety = state.origin.y;
 
-    context.strokeStyle = STROKE_VECTOR;
-    context.beginPath();
-
+    context.strokeStyle = stroke;
+    
     // The line
-    context.moveTo(start.x+offsetx, start.y+offsety);
-    context.lineTo(start.x+offsetx + x, start.y+offsety - y);
+    context.beginPath();
+    context.moveTo(start.x+offsetx, -(start.y)+offsety);
+    context.lineTo(start.x+offsetx + x, -(start.y)+offsety - y);
 
-    // The arrowhead. This should be a right angle, with the line at 45 degrees
-    
-    
     context.closePath();
     context.stroke();
     context.strokeStyle = STROKE_DEFAULT;
@@ -90,9 +117,22 @@ function pxstringToInt(input) {
     return parseInt(trimmedString);
 }
 
+// In degrees
+function rotateVector(x, y, angle) {
+    let cycles = (angle%360) / 360;
+    let angle_rad = 2*Math.PI*cycles;
+    
+    let rotation_matrix =
+	[Math.cos(angle_rad), -Math.sin(angle_rad),
+	 Math.sin(angle_rad), Math.cos(angle_rad)];
+
+    let result = {x: (rotation_matrix[0] * x + rotation_matrix[1] * y),
+		  y: (rotation_matrix[2] * x + rotation_matrix[3] * y)};
+
+    return result;
+}
 
 const init = () => {
-    console.log("Initialising canvas!");
     let handle = document.querySelector("#main_canvas");
     let context = handle.getContext('2d');
     let state = {
@@ -105,8 +145,8 @@ const init = () => {
 	objects: [
 	    {
 		type: 'vector',
-		x: 100,
-		y: 150,
+		x: 150,
+		y: 100,
 		start: {
 		    x: 0,
 		    y: 0
@@ -114,6 +154,7 @@ const init = () => {
 	    }
 	]
     };
+
     render(state);
 }
 
